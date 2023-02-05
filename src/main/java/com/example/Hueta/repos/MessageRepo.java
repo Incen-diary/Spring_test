0b8cac064f157f409a0e3e578c297ace1a3a6a4f
Insen_diary <vmoskaltsev.work@gmail.com>
2023-02-06 01:49:33 +0200

    1

---
+++ src/main/java/com/example/Hueta/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.Hueta.repos;
+
+import com.example.Hueta.Domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
